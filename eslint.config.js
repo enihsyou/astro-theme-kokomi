16430fdfbc03cb9323280af2dc4aaf4ed737eb2e
九条涼果 <enihsyou@gmail.com>
2025-05-08 02:39:14 +0800

    Add eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,32 @@
 import js from "@eslint/js";
 import globals from "globals";
+import astro from "eslint-plugin-astro";
 import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-    { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
-    { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
-    tseslint.configs.recommended,
+    globalIgnores(["**/.astro"], "ignore/astro-cache"),
+    globalIgnores(["*.config.{js,cjs,mjs}"], "ignore/config"),
+    {
+        name: "javascript",
+        files: ["**/*.{js,cjs,mjs}"],
+        plugins: { js },
+        rules: js.configs.recommended.rules,
+        languageOptions: { globals: globals.browser },
+    },
+    {
+        name: "typescript",
+        files: ["**/*.{ts,tsx}"],
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        extends: tseslint.configs.strictTypeChecked,
+    },
+    {
+        name: "astro",
+        extends: astro.configs.recommended,
+    },
 ]);
